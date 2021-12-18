7ec6d1ffd386f0028580e7a23cb43e54b3c17733
Zhankui <403019647@qq.com>
2021-12-18 22:53:20 +0800

    first commit  333

--- src/main/java/com/atguigu/rest/dao/EmployeeDao.java
+++ src/main/java/com/atguigu/rest/dao/EmployeeDao.java
@@ -19,7 +19,7 @@ public class EmployeeDao {
 //        employees.put(1005, new Employee(1005, "E-EE", "ee@163.com", 1));
 //    }
 
-    private static Integer initId = 1006;
+    private static Integer initId = 100666666;
 
     public void save(Employee employee){
         if(employee.getId() == null){
