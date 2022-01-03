f4e5ce7fd79e45533cfe21d79f167bb2033d6b8f
Zhankui <403019647@qq.com>
2022-01-03 22:37:23 +0800

    last commit  已全部完成增删改查，但是做的模糊查询显示时候切换页码有问题，因为页面还是以请求方式每次刷新返回页面，没有用ajax通过jquery在后面追加数据，所以每次都会刷新页面 其他的查询条件也跟着变了。前端的页面用ajax请求拼接html太麻烦了就没弄了，后续学到了更好的前端框架考虑替换下。。。   2022/1/1

---
+++ src/main/java/com/atguigu/rest/bean/UserInterface.java
@@ -0,0 +1,9 @@
+package com.atguigu.rest.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserInterface {
+
+    public String test();
+}
---
+++ src/main/java/com/atguigu/rest/service/MybatisDaoImplimert.java
@@ -0,0 +1,13 @@
+package com.atguigu.rest.service;
+
+import com.atguigu.rest.bean.Employee;
+import com.atguigu.rest.dao.MybatisDao;
+import com.atguigu.rest.utils.MyBatisUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisDaoImplimert   {
+
+
+}
